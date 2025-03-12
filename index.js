14500bf3c999b478e9b7ecc95b7bfa2e9adbfc3c
zl0ty <pilchu87@gmail.com>
2025-03-12 12:41:07 +0100

    Fix to align with deprecated Bitbucket API

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const bunyan = require('bunyan');
 const got = require('got');
 
-const { BITBUCKET_USERNAME, BITBUCKET_PASSWORD, RENOVATE_BOT_USER } =
+const { BITBUCKET_USERNAME, BITBUCKET_PASSWORD, BITBUCKET_WORKSPACE, RENOVATE_BOT_USER } =
   process.env;
 const MANUAL_MERGE_MESSAGE = 'merge this manually';
 const AUTO_MERGE_MESSAGE = '**Automerge**: Enabled.';
@@ -39,7 +39,7 @@ function isAutomerging(pr) {
 }
 
 function getPullRequests() {
-  const prEndpoint = `/2.0/pullrequests/${RENOVATE_BOT_USER}`;
+  const prEndpoint = `/2.0/workspaces/${BITBUCKET_WORKSPACE}/pullrequests/${RENOVATE_BOT_USER}`;
   log.info('Requesting %s%s...', DEFAULT_OPTIONS.prefixUrl, prEndpoint);
 
   return got.paginate('', {
@@ -74,9 +74,9 @@ function approvePullRequest(prHref) {
 }
 
 async function main() {
-  if (!BITBUCKET_USERNAME || !BITBUCKET_PASSWORD || !RENOVATE_BOT_USER) {
+  if (!BITBUCKET_USERNAME || !BITBUCKET_PASSWORD || !BITBUCKET_WORKSPACE || !RENOVATE_BOT_USER) {
     log.fatal(
-      'At least one of BITBUCKET_USERNAME, BITBUCKET_PASSWORD, RENOVATE_BOT_USER environement variables is not set.'
+      'At least one of BITBUCKET_USERNAME, BITBUCKET_PASSWORD, BITBUCKET_WORKSPACE, RENOVATE_BOT_USER environement variables is not set.'
     );
     process.exit(1);
   }
